8a91a9b8a4db16ff92e88d83d501b89b113ca43c
unknown <rickiamine>
2021-05-30 14:11:52 +0200

    run kata with creating an account and printing statement

--- src/main/java/com/kata/bankaccount/BankaccountApplication.java
+++ src/main/java/com/kata/bankaccount/BankaccountApplication.java
@@ -1,13 +1,21 @@
 package com.kata.bankaccount;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.Instant;
 
 @SpringBootApplication
 public class BankaccountApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankaccountApplication.class, args);
+		Account account = new Account(new Statement());
+		AccountStatementPrinter printer = new AccountStatementPrinter();
+
+		account.deposit(1000, Instant.now().toString());
+		account.deposit(2000, Instant.now().toString());
+		account.withdrawal(500, Instant.now().toString());
+
+		account.printStatement(printer);
 	}
 
 }
