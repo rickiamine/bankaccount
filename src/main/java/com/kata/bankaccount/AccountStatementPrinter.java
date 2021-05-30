9042bbcc7dc1a6f094a0bd1c37d5176615409a6f
unknown <rickiamine>
2021-05-30 14:10:07 +0200

    fix unit test printBalanceLine by implementing method in AccountStatementPrinter

--- src/main/java/com/kata/bankaccount/AccountStatementPrinter.java
+++ src/main/java/com/kata/bankaccount/AccountStatementPrinter.java
@@ -2,5 +2,6 @@ package com.kata.bankaccount;
 
 public class AccountStatementPrinter {
     public void println(BalanceLine balanceLine) {
+        System.out.println(balanceLine.getOperation().getDate()+";"+balanceLine.getOperation().getName()+";"+balanceLine.getOperation().getAmount()+";"+balanceLine.getBalanceAfterOperation());
     }
 }
