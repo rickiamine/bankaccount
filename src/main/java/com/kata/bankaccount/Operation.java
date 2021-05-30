7e37f77501121cd9c50fdeca07a17df2a64fdab1
unknown <rickiamine>
2021-05-30 13:43:57 +0200

    refactor operation by adding OperationType enum

--- src/main/java/com/kata/bankaccount/Operation.java
+++ src/main/java/com/kata/bankaccount/Operation.java
@@ -4,27 +4,31 @@ import java.util.Objects;
 
 public class Operation {
 
-    private String name;
+    private OperationType name;
     private int amount;
     private String date;
 
-    public Operation(String name, int amount, String date) {
+    public Operation(OperationType name, int amount, String date) {
         this.name = name;
         this.amount = amount;
         this.date = date;
     }
-
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Operation operation = (Operation) o;
-        return amount == operation.amount && Objects.equals(name, operation.name) && Objects.equals(date, operation.date);
+        return amount == operation.amount && name == operation.name && Objects.equals(date, operation.date);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(name, amount, date);
     }
+
+    enum OperationType {
+        DEPOSIT,WITHDRAWAL
+    }
+
 }
