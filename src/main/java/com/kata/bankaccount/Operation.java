e13edbff7697f9fc19870ce30d5b337a8d96301d
unknown <rickiamine>
2021-05-30 13:34:48 +0200

    create equals and hashcode method to fix first unit test of deposit operation

--- src/main/java/com/kata/bankaccount/Operation.java
+++ src/main/java/com/kata/bankaccount/Operation.java
@@ -1,6 +1,30 @@
 package com.kata.bankaccount;
 
+import java.util.Objects;
+
 public class Operation {
-    public Operation(String deposit, int amount, String date) {
+
+    private String name;
+    private int amount;
+    private String date;
+
+    public Operation(String name, int amount, String date) {
+        this.name = name;
+        this.amount = amount;
+        this.date = date;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Operation operation = (Operation) o;
+        return amount == operation.amount && Objects.equals(name, operation.name) && Objects.equals(date, operation.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, amount, date);
     }
 }
