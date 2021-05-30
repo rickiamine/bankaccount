d93f9ff4a14e359a296aaa99a313631278fe061f
unknown <rickiamine>
2021-05-30 14:01:05 +0200

    create equals and hashcod in BalanceLine to fix printDepositOperation unit test in Statement

--- src/main/java/com/kata/bankaccount/BalanceLine.java
+++ src/main/java/com/kata/bankaccount/BalanceLine.java
@@ -1,6 +1,26 @@
 package com.kata.bankaccount;
 
+import java.util.Objects;
+
 public class BalanceLine {
-    public BalanceLine(Operation operation, int amount) {
+    private Operation operation;
+    private int balanceAfterOperation;
+
+    public BalanceLine(Operation operation, int balanceAfterOperation) {
+        this.operation = operation;
+        this.balanceAfterOperation = balanceAfterOperation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BalanceLine that = (BalanceLine) o;
+        return balanceAfterOperation == that.balanceAfterOperation && Objects.equals(operation, that.operation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(operation, balanceAfterOperation);
     }
 }
