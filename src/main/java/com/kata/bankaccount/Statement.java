d4a724aa9fdbd7c523ed1eade205643194c9efd1
unknown <rickiamine>
2021-05-30 13:57:18 +0200

    implement exportToPrint method in Statement to fix printDepositOperation test

--- src/main/java/com/kata/bankaccount/Statement.java
+++ src/main/java/com/kata/bankaccount/Statement.java
@@ -1,8 +1,15 @@
 package com.kata.bankaccount;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Statement {
-    public void addNewBalance(Operation deposit, int balance) {
-        throw new UnsupportedOperationException();
+
+    private List<BalanceLine > balanceLines = new LinkedList<>();
+
+
+    public void addNewBalance(Operation operation, int balanceAfterOperation) {
+        this.balanceLines.add(new BalanceLine(operation,balanceAfterOperation));
     }
 
     public void exportToPrint(AccountStatementPrinter printer) {
