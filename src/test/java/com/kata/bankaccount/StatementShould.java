7e3c1e34c43bcb0f598fddf94709430587e0fbd2
unknown <rickiamine>
2021-05-30 13:51:49 +0200

    create unit test failed for printing deposit operation

---
+++ src/test/java/com/kata/bankaccount/StatementShould.java
@@ -0,0 +1,35 @@
+package com.kata.bankaccount;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.Instant;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StatementShould {
+
+    private Statement statement;
+
+    @Mock
+    AccountStatementPrinter printer;
+
+    @Before
+    public void init() {
+        statement = new Statement();
+    }
+
+    @Test
+    public void printDepositOperation() {
+        int amount= 100;
+        String date = Instant.now().toString();
+        statement.addNewBalance(new Operation(Operation.OperationType.DEPOSIT,amount,date),amount);
+        statement.exportToPrint(printer);
+        verify(printer).println(new BalanceLine(new Operation(Operation.OperationType.DEPOSIT,amount,date),amount));
+    }
+
+}
